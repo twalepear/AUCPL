688033879fd95bcff7942e9c6aec86729afa4567
Thoa Thanh Thi Le <a1847933@adelaide.edu.au>
2026-03-24 17:47:51 +1030

    check

--- clocking_chaos.cpp
+++ clocking_chaos.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
 
 int main(){
 	int N;
@@ -14,12 +15,16 @@ int main(){
 	}
 
 	int max_C = 0;
+	float max_V = 0;
 	for (int i = 0; i < N; i++){
 		if (C[i] > max_C){
 			max_C = C[i];
 		}
+		if (V[i] > max_V){
+			max_V = V[i];
 	}
-	
-	
+	float result = max_C * max_V;
+	std::cout << std::fixed << std::setprecision(3) << result << std::endl;
+
 	return 0;
 }
